0cb035f8584a796de3cb072473e5ed8b8d47f375
Scott Hedrick <me@scottshedrick.com>
2018-01-18 14:35:01 -0600

    final solution

--- exercise1_quiz/main.go
+++ exercise1_quiz/main.go
@@ -5,33 +5,19 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
-// Read in a file (csv) ✅
-// Present the quiz to the user
-// Keep track of how many questions they get right and how many incorrect
-// Immediately ask the next question whether the previous is wrong/right
-// Default to problems.csv but user should be able to customize the file via
-// a flag
-
-// CSV format:
-// 5+5, 10
-// 7+3, 10
-// 1+1, 2
-// etc
-
-// Assume quizzes are < 100 questions and have single word/number answers
-
-// At the end, should output total number of questions correct
-// and how many questions there were in total (x/y correct)
-// Invalid answers are considered incorrect.
+var (
+	csvFileName = flag.String("csv", "problems.csv",
+		"a csv file in the format of 'question,answer'")
+	timeLimit = flag.Int("limit", 30, "the time limit for the quiz in seconds")
+)
 
 func main() {
 
-	csvFileName := flag.String("csv", "problems.csv",
-		"a csv file in the format of 'question,answer'")
 	flag.Parse()
-
 	file, err := os.Open(*csvFileName)
 	if err != nil {
 		exit(fmt.Sprintf("Failed to open CSV file: %s", *csvFileName))
@@ -44,13 +30,26 @@ func main() {
 	}
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
+
 	for i, p := range problems {
 		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 
@@ -62,7 +61,7 @@ func parseLines(lines [][]string) []problem {
 	for i, line := range lines {
 		ret[i] = problem{
 			q: line[0],
-			a: line[1],
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
